342f67815a46a9d1b6229b784b1a713bf8834e20
jamy-dude <jamesstanford2002@gmail.com>
2024-07-03 22:28:16 +0500

    Revert "1. Added pagination for blogs with 3 blogs per page limit"
    This reverts commit ea6438b281794b6f26038c099d212280ae5c244e.

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Post, Comment
+from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -12,10 +12,5 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
 
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ['name', 'email', 'post', 'created', 'active']
-    list_filter = ['active', 'created', 'updated']
-    search_fields = ['name', 'email', 'body']
 # Register your models here.
 # admin.site.register(Post)
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -17,8 +16,7 @@ class Post(models.Model):
 
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250,
-                            unique_for_date='publish')
+    slug = models.SlugField(max_length=250)
     author = models.ForeignKey(User,
                                 on_delete=models.CASCADE,
                                 related_name='blog_posts',)
@@ -41,34 +39,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
-    # def get_absolute_url(self):
-    #     return reverse('blog:post_detail',
-    #                    args=[self.id])
-
-    def get_absolute_url(self):
-        return reverse('blog:post_detail',
-                       args=[self.publish.year,
-                             self.publish.month,
-                             self.publish.day,
-                             self.slug])
-
-class Comment(models.Model):
-    post = models.ForeignKey(Post,
-                             on_delete=models.CASCADE,
-                             related_name='comments',)
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
-    body = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    active = models.BooleanField(default=True)
-
-    class Meta:
-        ordering = ['created']
-        indexes = [
-            models.Index(fields=['created']),
-        ]
-
-    def __str__(self):
-        return f'Comment by {self.name} on {self.post}'
